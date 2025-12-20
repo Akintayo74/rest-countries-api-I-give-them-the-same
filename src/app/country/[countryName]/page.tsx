8e9c5da69f6f18d5a85b3f44f0c3124d18780852
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-12-20 14:57:42 +0100

    Refactor dynamic segments to use git bot chore data

--- src/app/country/[countryName]/page.tsx
+++ src/app/country/[countryName]/page.tsx
@@ -1,13 +1,17 @@
-import CountryDisplay from "@/components/CountryDisplay";
 import * as React from "react";
+import countriesData from '@/../data/countries.json'
 
 async function countryCode({ params }) {
   const { countryName } = await params;
 
-  // const {data, error, isLoading} = useCountryDisplay(countryName);
-  return <CountryDisplay countryName={countryName} />
+  const country = countriesData.find(c => c.cca3 === countryName)
 
-  
+  console.log(countriesData)
+  return (
+    <div>
+      <h1>{country.capital}</h1>
+    </div>
+  );
 }
 
 export default countryCode;
