43c01fab4972cc6777144a9fcc10b462073418b3
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-12-20 15:30:17 +0100

    Finish refactor of select-region to use local data

--- src/components/SelectRegion/SelectRegion.tsx
+++ src/components/SelectRegion/SelectRegion.tsx
@@ -29,7 +29,7 @@ function SelectRegion() {
 
           <optgroup label="Regions">
             <option value="africa">Africa</option>
-            <option value="america">America</option>
+            <option value="americas">Americas</option>
             <option value="asia">Asia</option>
             <option value="europe">Europe</option>
             <option value="oceania">Oceania</option>
